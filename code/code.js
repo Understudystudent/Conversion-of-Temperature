f22b5b433f241d9bc1a6c5f1da4f36ebc64446b4
Carl James <carlmjames2@gmail.com>
2023-11-13 13:15:54 -0500

    added Javascript

--- code/code.js
+++ code/code.js
@@ -1,5 +1,19 @@
-const celcius = document.querySelector('data-celcius');
-const kelvin = document.querySelector('data-kelvin');
-const fahrenheit = document.querySelector('data-fahrenheit');
+const fahrenheitInput = document.querySelector("#fahrenheit");
+const CelsiusInput = document.querySelector("Celsius");
+const inputs = document.getElementsByClassName("input");
 
-const input = document.getElementsByClassName('input');
+for (let i = 0; i < inputs.length; i++) {
+    let input = inputs[i];
+
+    input.addEventListener("input", function (e) {
+        let value = parseFloat(e.target.value);
+        switch (e.target.name) {
+            case "fahrenheit":
+                CelsiusInput.value = (value - 32) / 1.8;
+                break;
+            case "Celsius":
+                fahrenheitInput.value = (value * 1.8) + 32;
+                break;
+        }
+    });
+}
